e7e76b9c5cbfd549f7dca1c52ca60e587534e59d
krahul <culerrahul2024@gmail.com>
2026-01-15 23:44:24 +0530

    learnt some more about java threads

--- Threads.java
+++ Threads.java
@@ -4,6 +4,41 @@ public class Threads {
             MyThread mt = new MyThread(i);
             mt.run();
         }
+
+        for (int i = 1; i <= 2; i++) {
+            Task t = new Task();
+            t.run();
+        }
+
+        try {
+            threadLifeCycleDemo();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void threadLifeCycleDemo () throws InterruptedException {
+        Thread t = new Thread(() -> {
+            try {
+                System.out.println("Thread running");
+                Thread.sleep(2000);
+                System.out.println("Thread finishing");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        System.out.println("State: " + t.getState()); // new
+
+        t.start();
+        System.out.println("State: " + t.getState()); // runnable/running sort of
+
+        Thread.sleep(1000);
+        System.out.println("State: " + t.getState()); // timed_wait
+
+        t.join(); // waiting for thread completion
+        System.out.println("State: " + t.getState()); // termination
     }
 }
 
@@ -21,5 +56,34 @@ class MyThread extends Thread {
             Thread.sleep(100);
             System.out.println("running the thread no. : " + this.threadNum);
         } catch (Exception e) { }
+    }
+}
+
+//NOTE: Another way, preferred
+//
+// since there is no way to do multiple inheritance,
+// so if a class already extends another class, it is
+// not possible to extend class Thread, it is better to
+// implement runnable interface alongside any interface or class
+
+//NOTE: Thread life cycle
+// new -> created but not started
+// runnable -> thread is ready to run or is running
+// blocked -> blocked either by locks or some way
+// waiting -> waiting for another thread
+// timed_waiting
+// terminated
+
+class Task implements Runnable {
+    @Override
+    public void run() {
+         for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + ", count = " + i);
+            try {
+                Thread.sleep(100);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
