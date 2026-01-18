2fc23ff2c6ad054ea83f584b1c2c0ec4d3bbc399
krahul <culerrahul2024@gmail.com>
2026-01-18 23:28:35 +0530

    some more things about threads

--- Threads.java
+++ Threads.java
@@ -1,20 +1,8 @@
 public class Threads {
     public static void main(String[] a) {
-        for (int i = 1; i <= 10; i++) {
-            MyThread mt = new MyThread(i);
-            mt.run();
-        }
-
-        for (int i = 1; i <= 2; i++) {
-            Task t = new Task();
-            t.run();
-        }
-
-        try {
-            threadLifeCycleDemo();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // runInitialThreadExamples();
+        raceConditionDemo();
+        fixedRaceConditionDemo();
     }
 
     static void threadLifeCycleDemo () throws InterruptedException {
@@ -40,7 +28,114 @@ public class Threads {
         t.join(); // waiting for thread completion
         System.out.println("State: " + t.getState()); // termination
     }
+
+    static void runInitialThreadExamples () {
+        for (int i = 1; i <= 10; i++) {
+            MyThread mt = new MyThread(i);
+            mt.run();
+        }
+
+        for (int i = 1; i <= 2; i++) {
+            Task t = new Task();
+            t.run();
+        }
+
+        try {
+            threadLifeCycleDemo();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void raceConditionDemo() {
+        Counter counter = new Counter();
+
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+        Thread t3 = new Thread(task);
+        Thread t4 = new Thread(task);
+
+        try {
+            t1.start();
+            t2.start();
+            t3.start();
+            t4.start();
+
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Count = " + counter.getCount());
+    }
+
+    static void fixedRaceConditionDemo() {
+        Counter c = new Counter();
+
+        Runnable task = () -> {
+            for (int i = 1; i <= 1000; i++) {
+                c.incrementSync();
+            }
+        };
+
+        int nThreads = 4;
+        Thread[] threads = new Thread[nThreads];
+
+        for (int i = 0; i < nThreads; i++) {
+            threads[i] = new Thread(task);
+            threads[i].start();
+        }
+
+        try {
+            for (int i = 0; i < nThreads; i++) {
+                threads[i].join();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Count(Expected = 4000) with sync = " + c.getCountSync());
+    }
 }
+
+class Counter {
+    private int count = 0;
+
+    public void increment() {
+        count++; // this is three step op -> read count, add 1 to count, write back to count
+    }
+
+    // NOTE: couple more ways
+    // AtomicInteger, LongAdder
+    // Locks, Semaphores
+    // ReadWriteLock
+    //
+    // synchronized -> built-in mutexes
+    // ReentrantLock -> cpp std::mutex
+
+    public synchronized void incrementSync() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public synchronized int getCountSync() {
+        return count;
+    }
+}
+
+//----------------------------- Basic Tutorial on usage of thread/runnable ------------------------------
 
 // one way to use
 class MyThread extends Thread {
@@ -77,7 +172,7 @@ class MyThread extends Thread {
 class Task implements Runnable {
     @Override
     public void run() {
-         for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(Thread.currentThread().getName() + ", count = " + i);
             try {
                 Thread.sleep(100);
